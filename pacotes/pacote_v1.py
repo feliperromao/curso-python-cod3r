5791be24d10017ea519a2e9f58bf1a81eb75d32c
Felipe Romao <feliperomaocad@gmail.com>
2019-04-23 23:29:12 -0300

    Pacotes

---
+++ pacotes/pacote_v1.py
@@ -0,0 +1,6 @@
+#!C:\Users\felipe\AppData\Local\Programs\Python\Python37-32\python
+
+from pacote1 import modulo1
+
+print(type(modulo1))
+print(modulo1.soma(10, 3))
---
+++ pacotes/pacote_v2.py
@@ -0,0 +1,5 @@
+#!C:\Users\felipe\AppData\Local\Programs\Python\Python37-32\python
+
+from pacote1 import modulo2
+
+modulo2.main()
