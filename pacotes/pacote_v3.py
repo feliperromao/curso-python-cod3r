d8b36bad9cff81ec91c57123b1ecf0f3940eeff0
Felipe Romao <feliperomaocad@gmail.com>
2019-04-24 23:52:49 -0300

    Pacotes

---
+++ pacotes/pacote_v3.py
@@ -0,0 +1,6 @@
+#!C:\Users\felipe\AppData\Local\Programs\Python\Python37-32\python
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
+
+print('Soma: ', modulo1.soma(2,3))
+print('Sub: ', modulo1_sub.sub(5, 1))
---
+++ pacotes/pacote_v4.py
@@ -0,0 +1,6 @@
+#!C:\Users\felipe\AppData\Local\Programs\Python\Python37-32\python
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import sub as subtracao
+
+print('Soma: ', soma(5, 10))
+print('Sub: ', subtracao(10, 5))
