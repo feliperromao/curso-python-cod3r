17413e78ab8cd34c9651c94ab73e6f41cefc5541
Felipe Romao <feliperomaocad@gmail.com>
2019-04-29 22:57:05 -0300

    Finalizando todo

--- poo/todo.py
+++ poo/todo.py
@@ -2,6 +2,10 @@
 
 from datetime import datetime, timedelta
 
+class TarefaNaoEncontrada(Exception):
+    pass
+
+
 class Projeto:
     def __init__(self, nome):
         self.nome = nome
@@ -9,6 +13,11 @@ class Projeto:
 
     def __iter__(self):
         return self.tarefas.__iter__()
+
+    def __iadd__(self, tarefa):
+        tarefa.dono = self
+        self._add_tarefa(tarefa)
+        return self
 
     def __str__(self):
         return f'{self.nome} ({ len(self.pendentes()) } tarefa(s) pendente(s))'
@@ -30,7 +39,10 @@ class Projeto:
         return [tarefa for tarefa in self.tarefas if not tarefa.feito]
     
     def procurar(self, descricao):
-        return [tarefa for tarefa in self.tarefas if tarefa.descricao == descricao][0]
+        try:
+            return [tarefa for tarefa in self.tarefas if tarefa.descricao == descricao][0]
+        except IndexError as e:
+            raise TarefaNaoEncontrada(str(e))
 
 
 class Tarefa:
@@ -60,11 +72,15 @@ class TarefaRecorrente(Tarefa):
     def __init__(self, descricao, vencimento, dias=7):
         super().__init__(descricao, vencimento)
         self.dias = dias
+        self.dono = None
 
     def concluir(self):
         super().concluir()
         novo_vencimento = datetime.now() + timedelta(days=self.dias)
-        return TarefaRecorrente(self.descricao, novo_vencimento, self.dias)
+        nova_tarefa = TarefaRecorrente(self.descricao, novo_vencimento, self.dias)
+        if self.dono:
+            self.dono += nova_tarefa
+        return nova_tarefa
 
 
 def main():
@@ -72,10 +88,15 @@ def main():
     casa.add('lavar louça', datetime.now() + timedelta(minutes=60))
     casa.add('lavar roupa', datetime.now() + timedelta(days=1))
     casa.add('lavar moto', datetime.now() + timedelta(days=3, minutes=10))
-    casa.add(TarefaRecorrente('limpar casa', datetime.now(), 7))
+    casa += TarefaRecorrente('limpar casa', datetime.now(), 7)
     casa.add(casa.procurar('limpar casa').concluir())
 
     casa.procurar('lavar louça').concluir()
+    
+    try:
+        casa.procurar('Pagar conta de internet').concluir()
+    except TarefaNaoEncontrada as e:
+        print(f'A causa foi: {e}')
 
     print(casa)
     for tarefa in casa:
